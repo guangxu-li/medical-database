7b635af328b07767352eea965439462995189f75
Guangxu Li <guangxu.daniel.li@gmail.com>
2019-11-23 17:49:25 -0500

    finish users/new.php and fixed bugs

--- private/database/query_functions.php
+++ private/database/query_functions.php
@@ -59,22 +59,16 @@
     function validate_user($user) {
         $errors = [];
 
-        $UID = (int) $user['UID'];
         $ufname = $user['ufname'];
         $ulname = $user['ulname'];
         $urole = $user['urole'];
         $did = (int) $user['did'];
 
-        // UID
-        if ($UID < 0 || $UID > 9999) {
-            $errors[] = "User ID must be an integer between 0 and 9999.";
-        }
-
         // ufname
         if (!is_string($ufname)) {
             $errors[] = "User's first name is required.";
         } else if (!ctype_alpha($ufname)) {
-            $errors[] = "User's first name should only consist of letters.";
+            $errors[] = "User's first name is required and should only consist of letters.";
         } else if (strlen($ufname) > 20) {
             $errors[] = "User's first name can't be longer than 20 letters.";
         }
@@ -83,7 +77,7 @@
         if (!is_string($ulname)) {
             $errors[] = "User's last name is required.";
         } else if (!ctype_alpha($ulname)) {
-            $errors[] = "User's last name should only consist of letters.";
+            $errors[] = "User's last name is required and should only consist of letters.";
         } else if (strlen($ulname) > 20) {
             $errors[] = "User's last name can't be longer than 20 letters.";
         }
@@ -92,17 +86,39 @@
         if (!is_string($urole)) {
             $errors[] = "User's role is required.";
         } else if (!ctype_alpha($urole)) {
-            $errors[] = "User's role should only consis of letters.";
+            $errors[] = "User's role is required and should only consis of letters.";
         } else if (strlen($urole) > 20) { 
             $errors[] = "User's role can't be longer than 20 letters.";
         }
 
-        // did
-        if ($did < 0 || $did > 99) {
-            $errors[] = "User's department id must be an integer between 0 and 99.";
+        return $errors;
+    }
+
+    function insert_user($user) {
+        global $db;
+
+        $errors = validate_user($user);
+        if(!empty($errors)) {
+            return $errors;
         }
 
-        return $errors;
+        $sql = "INSERT INTO users VALUES (NULL, ";
+        $sql .= "upper(\"" .db_escape($db, $user['ufname']) ."\"), ";
+        $sql .= "upper(\"" .db_escape($db, $user['ulname']) ."\"), ";
+        $sql .= "upper(\"" .db_escape($db, $user['urole']) ."\"), ";
+        $sql .= db_escape($db, $user['did']) .");";
+        echo $sql;
+        $result = mysqli_query($db, $sql);
+
+        if($result) {
+            // true
+            return true;
+        } else {
+            // false
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
     }
 
     function update_user($user) {
