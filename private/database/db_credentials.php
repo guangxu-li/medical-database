e3ab459109c303a9d0bd8fc554b294a5b520039a
Guangxu Li <guangxu.li@nyu.edu>
2019-11-24 13:05:19 -0500

    update db credentials for macos

--- private/database/db_credentials.php
+++ private/database/db_credentials.php
@@ -1,7 +1,7 @@
 <?php
-    define("DB_SERVER", "localhost:8889");
+    define("DB_SERVER", "localhost");
     define("DB_USER", "root");
-    define("DB_PASS", "T1MEsCHANGE.");
+    define("DB_PASS", "root.");
     define("DB_NAME", "F196083B");
     
 ?>
